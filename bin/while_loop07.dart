0b5e9974f749f9ae81774d7148bcecf2dc9eb348
sharof <sharofimamov3@gmail.com>
2022-08-12 13:18:29 +0500

    7

--- bin/while_loop07.dart
+++ bin/while_loop07.dart
@@ -8,18 +8,18 @@ int func(int a) {
   int s = 0;
   int z = 0;
   while (i < a) {
-    i += 1;
+    i = i + 1;
     if (i % 2 == 0) {
-      s += i;
+      s = s + i;
     }
     if (i % 2 == 1) {
       z += i;
     }
-    if (s < z) {
-      return s;
-    }
   }
-  return z;
+  if (z < s) {
+    return z;
+  }
+  return s;
 }
 
 void main() {
