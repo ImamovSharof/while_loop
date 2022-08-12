4bd00bc46ba06755ff7556ce4540a5ac53c8c74c
sharof <sharofimamov3@gmail.com>
2022-08-12 12:59:57 +0500

    7

--- bin/while_loop07.dart
+++ bin/while_loop07.dart
@@ -3,4 +3,25 @@
   Return odd and even numbers up to "a", whichever is smaller
   
 */
-void main() {}
+int func(int a) {
+  int i = 0;
+  int s = 0;
+  int z = 0;
+  while (i < a) {
+    i += 1;
+    if (i % 2 == 0) {
+      s += i;
+    }
+    if (i % 2 == 1) {
+      z += i;
+    }
+    if (s < z) {
+      return s;
+    }
+  }
+  return z;
+}
+
+void main() {
+  print(func(10));
+}
